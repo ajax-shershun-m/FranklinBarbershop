05ff940a571d6283818b732b0eedb144e4482629
Maksim Shershun <sm.shershun@gmail.com>
2021-11-21 14:28:48 +0200

    WIP

--- FranklinBarbershop/FranklinBarbershop/Modules/Profile/ProfileViewController.swift
+++ FranklinBarbershop/FranklinBarbershop/Modules/Profile/ProfileViewController.swift
@@ -13,8 +13,24 @@ class ProfileViewController: UIViewController {
 
     var viewModel: ProfileViewModelType!
 
+    private lazy var profileBackgroundImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "bc_profile")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = .black
+
+        self.view.addSubview(profileBackgroundImage)
+
+        NSLayoutConstraint.activate([
+            profileBackgroundImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            profileBackgroundImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            profileBackgroundImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            profileBackgroundImage.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 3)
+        ])
     }
 }
